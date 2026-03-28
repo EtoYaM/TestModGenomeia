ab3c2e4b8497d5c899f416d77a3ab56540fdc2d9
infinitybee0 <infinitybee0@gmail.com>
2026-03-28 21:02:32 +0300

    фикс

--- src/com/example/mod/Main.java
+++ src/com/example/mod/Main.java
@@ -1,5 +1,12 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+package com.example.mod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Main implements ModInitializer {
+    public static final String MOD_ID = "examplemod";
+
+    @Override
+    public void onInitialize() {
+        // Почему-то не работает
     }
 }
